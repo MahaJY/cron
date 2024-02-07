25c7e9703f23bcaa3ab5cdd15c654965b9409559
MahaJY <158135608+MahaJY@users.noreply.github.com>
2024-02-07 21:22:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/cron', authRoutes);
 sequelize.sync().then(() => {
-    const PORT = 2015;
+    const PORT = 2028;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
