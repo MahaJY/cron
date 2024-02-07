120bf411d6f24643d67d78e8d2ba23c2f920eeb7
MahaJY <158135608+MahaJY@users.noreply.github.com>
2024-02-07 21:24:59 +0530

    Update authroutes.js

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const mainController = require('../controllers/maincontroller');
-const JWTUtils = require('../utils/jwtutils')
 
 const router = express.Router();
 router.get('/user',mainController.getAllUsers);
-router.post('/upload',mainController.cronjob);
+router.post('/upload',mainController.uploadJob);
 router.get('/export',mainController.exportExcel);
 module.exports = router;
